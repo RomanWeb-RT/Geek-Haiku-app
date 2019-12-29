21c90e1d8b1bb31d19651b22db1015359807041e
RomanWeb-RT <59302800+romanweb-rt@users.noreply.github.com>
2019-12-29 13:13:28 +0300

    Add Input and Button components. Create HaikuCreator container with input fields and validation on empty field

--- geek-haiku-front/src/App.js
+++ geek-haiku-front/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import {Route, Switch} from "react-router-dom";
 import Main from "./containers/Main/Main";
 import About from "./containers/About/About";
+import HaikuCreator from "./containers/HaikuCreator/HaikuCreator";
 
 function App() {
     return (
         <Layout>
             <Switch>
                 <Route path={'/about'} component={About}/>
+                <Route path={'/create'} component={HaikuCreator}/>
                 <Route path={'/'} component={Main}/>
             </Switch>
         </Layout>
---
+++ geek-haiku-front/src/containers/HaikuCreator/HaikuCreator.js
@@ -0,0 +1,109 @@
+import classes from './HaikuCreator.css'
+import React, {Component} from "react";
+import Button from "../../components/Ui/Button/Button";
+import Input from "../../components/Ui/Input/Input";
+import Haiku from "../../components/Haiku/Haiku";
+import {createControl, validate, validateForm} from "../../form/formFramework";
+
+class HaikuCreator extends Component {
+    state = {
+        haiku: [],
+        isFormValid: false,
+        formInputs: {
+            firstLine: createControl({
+                label: 'Первая строка (5 слогов)',
+                invalidMessage: 'Поле не должно быть пустым',
+                id: 1
+            }, {required: true}),
+            secondLine: createControl({
+                label: 'Вторая строка (7 слогов)',
+                invalidMessage: 'Поле не должно быть пустым',
+                id: 2
+            }, {required: true}),
+            thirdLine: createControl({
+                label: 'Третья строка (5 слогов)',
+                invalidMessage: 'Поле не должно быть пустым',
+                id: 3
+            }, {required: true}),
+        }
+    };
+
+    submitHandler = event => {
+        event.preventDefault();
+    };
+
+    createHaikuHandler = async (event) => {
+        event.preventDefault();
+        const haiku = this.state.haiku.concat();
+        const index = haiku.length + 1
+        const {firstLine, secondLine, thirdLine} = this.state.formInputs;
+        const result = {
+            text: [
+                {text: firstLine.value, id: firstLine.id},
+                {text: secondLine.value, id: secondLine.id},
+                {text: thirdLine.value, id: thirdLine.id}
+            ]
+        };
+        haiku.push(result);
+
+        try {
+            await fetch('')
+        } catch (e) {
+            console.error(e)
+        }
+    };
+
+    changeHandler = (value, controlName) => {
+        const formInputs = {...this.state.formInputs};
+        const control = {...formInputs[controlName]};
+
+        control.touched = true;
+        control.value = value;
+        control.valid = validate(control.value, control.validation);
+
+        formInputs[controlName] = control;
+
+        this.setState({
+            formInputs,
+            isFormValid: validateForm(formInputs)
+        })
+    };
+
+    inputFieldsRender() {
+        return Object.keys(this.state.formInputs).map((controlName) => {
+            const control = this.state.formInputs[controlName];
+            return (
+                <Input
+                    label={control.label}
+                    value={control.value}
+                    valid={control.valid}
+                    shouldValidate={!!control.validation}
+                    touched={control.touched}
+                    invalidMessage={control.invalidMessage}
+                    onChange={event => this.changeHandler(event.target.value, controlName)}
+                />)
+        })
+    }
+
+    render() {
+        return (
+            <div className={classes.HaikuCreator}>
+                <div>
+                    <h1>Ваше Хайку!</h1>
+                    <form onSubmit={this.submitHandler}>
+                        {this.inputFieldsRender()}
+                        <hr/>
+                        <h2>Предпросмотр</h2>
+                        <Haiku/>
+                        <hr/>
+                        <Button type="primary" onClick={this.createHaikuHandler} disabled={!this.state.isFormValid}>
+                            Create
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default HaikuCreator;
