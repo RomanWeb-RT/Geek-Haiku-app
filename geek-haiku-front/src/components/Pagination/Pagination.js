14523f6a402906873a2b0391b4c5e5f0e4248f2e
RomanWeb-RT <59302800+romanweb-rt@users.noreply.github.com>
2020-01-09 21:51:40 +0300

    Create Pagination component

---
+++ geek-haiku-front/src/components/Pagination/Pagination.js
@@ -0,0 +1,12 @@
+import styles from './Pagination.css'
+import React from "react";
+
+const Pagination = props =>{
+
+
+    return (
+        <div className={styles.Pagination}>
+
+        </div>
+    )
+};
